2d76c28edd0c803bed9e37d37226eaf1a8bf62c0
Andread <aotm2000@gmail.com>
2023-11-30 22:12:37 -0300

    Añadir main.js y cambios al index

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const listaRepositorios = document.getElementById("lista-repositorios");
+
+    fetch("https://api.github.com/users/tunombre/repos")
+        .then(response => response.json())
+        .then(repos => {
+            repos.forEach(repo => {
+                const listItem = document.createElement("li");
+                listItem.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a> - ${repo.description}`;
+                listaRepositorios.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error("Error al obtener repositorios", error));
+});
